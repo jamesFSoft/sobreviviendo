9a9f31a6e23f7c184dff0243862c4f59ecb69936
LuisaMalaver <maquetadorweb@booksandbooksdigital.com.co>
2021-06-13 17:15:41 -0500

    se agrega comentarios a la clase fruta

--- src/application/clases/Fruta.java
+++ src/application/clases/Fruta.java
@@ -1,20 +1,40 @@
 package application.clases;
 
 public class Fruta extends Alimento{
-	
+	/**
+	 * Atributos principales de la clase
+	 */
 	private Boolean vitamina;
+
+	/**
+	 *
+	 * @param nombre
+	 * @param puntaje
+	 * @param vitamina
+	 * constructor que recibe los parametros de nombre, puntaje y vitamina
+	 */
 
 	public Fruta(String nombre, Integer puntaje, Boolean vitamina) {
 		super(nombre, puntaje);
 		this.vitamina = vitamina;
 	}
 
+	/**
+	 *
+	 * @return el atributo de vitamina
+	 */
+
 	public Boolean getVitamina() {
 		return vitamina;
 	}
 
+	/**
+	 * @param vitamina
+	 * resibe el valor para ser asignado al atributo vitamina
+	 */
+
 	public void setVitamina(Boolean vitamina) {
 		this.vitamina = vitamina;
 	}
-	
+
 }
