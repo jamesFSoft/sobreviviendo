37c990a020aa5c35a72bb0887180473a2c82f2e0
LuisaMalaver <maquetadorweb@booksandbooksdigital.com.co>
2021-06-13 17:13:47 -0500

    se agregan comentarios a la clase carne

--- src/application/clases/Carne.java
+++ src/application/clases/Carne.java
@@ -1,8 +1,17 @@
 package application.clases;
 
 public class Carne extends Alimento{
-
+	/**
+	 * Atributo principal de la clase
+	 */
 	private Boolean cocinado;
+
+	/**
+	 * @param nombre
+	 * @param puntaje
+	 * @param cocinado
+	 * constructor que recibe los parametros de nombre, puntaje y cocinado
+	 */
 
 	public Carne(String nombre, Integer puntaje, Boolean cocinado) {
 		super(nombre, puntaje);
@@ -14,10 +23,19 @@ public class Carne extends Alimento{
 		this.cocinado = cocinado;
 	}
 
+	/**
+	 * @return el atributo de cocinado
+	 */
+
 	public Boolean getCocinarse() {
-		
+
 		return cocinado;
 	}
+
+	/**
+	 * @param cocinado
+	 * resibe el valor para ser asignado al atributo cocinado
+	 */
 
 	public void setCocinarse(Boolean cocinado) {
 		this.cocinado = cocinado;
