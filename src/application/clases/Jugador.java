33cc5a7f6a78c75a9690d1e4b51be40d2887c4b2
LuisaMalaver <maquetadorweb@booksandbooksdigital.com.co>
2021-06-15 17:45:11 -0500

    se ajustan comentarios en la clase jugador

--- src/application/clases/Jugador.java
+++ src/application/clases/Jugador.java
@@ -6,16 +6,18 @@ public class Jugador {
 	 * Atributos principales de la clase
 	 */
 	private String nombre;
-	
+
 	/**
-	 * Retorna el nombre del jugador
+	 * @return el atributo nombre
 	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+
 	/**
-	 * Establece el nombre del jugador
+	 * @param nombre
+	 * resibe el valor para ser asignado al nombre
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
