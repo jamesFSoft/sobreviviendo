c277632a545291d7b12b0741d5428cdbedb12e58
h3nryp3 <54733258+h3nryp3@users.noreply.github.com>
2021-06-14 22:10:35 -0500

    Update Jugador.java

--- src/application/clases/Jugador.java
+++ src/application/clases/Jugador.java
@@ -1,13 +1,22 @@
 package application.clases;
 
 public class Jugador {
-	
-	private String nombre;
 
+	/**
+	 * Atributos principales de la clase
+	 */
+	private String nombre;
+	
+	/**
+	 * Retorna el nombre del jugador
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Establece el nombre del jugador
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
