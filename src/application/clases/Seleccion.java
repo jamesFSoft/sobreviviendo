6bf820addfbf0bd816e9cc41da6214c45bfe4353
LuisaMalaver <maquetadorweb@booksandbooksdigital.com.co>
2021-06-15 17:49:25 -0500

    se agregan comentarios a las clases faltantes

--- src/application/clases/Seleccion.java
+++ src/application/clases/Seleccion.java
@@ -1,13 +1,23 @@
 package application.clases;
 
 public class Seleccion {
-	
+	/**
+	 * Atributo principal de la clase
+	 */
 	private char opcion;
+
+	/**
+	 * @return el atributo opcion
+	 */
 
 	public char getOpcion() {
 		return opcion;
 	}
 
+	/**
+	 * @param opcion
+	 * resibe el valor para ser asignado al atributo opcion
+	 */
 	public void setOpcion(char opcion) {
 		this.opcion = opcion;
 	}
