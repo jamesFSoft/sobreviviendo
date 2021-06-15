b0b2ddceb20f66a4c4c278bd327efc5243719ca6
LuisaMalaver <maquetadorweb@booksandbooksdigital.com.co>
2021-06-15 17:49:10 -0500

    se ajustan comentarios clase nocivo

--- src/application/clases/Nocivo.java
+++ src/application/clases/Nocivo.java
@@ -1,27 +1,38 @@
 package application.clases;
 /**
-    * Clase creada para el alimento Nocivo hereda de la Super class Alimento  
+    * Clase creada para el alimento Nocivo hereda de la Super class Alimento
 */
 public class Nocivo extends Alimento{
-	
+
 	/**
-	 * Atributos principales de la clase
+	 * Atributo principal de la clase
 	 */
 	private Integer dolor;
 
 	/**
-        * Constructor de la clase, recibe parametros propios y heredados 
-    	*/
+	 *
+	 * @param nombre
+	 * @param puntaje
+	 * @param dolor
+     * Constructor de la clase, recibe parametros propios y heredados
+	 */
 	public Nocivo(String nombre, Integer puntaje, Integer dolor) {
 		super(nombre, puntaje);
 		this.puntaje = puntaje + dolor;
 		this.dolor = dolor;
 	}
 
+	/**
+	 * @return el atributo de dolor
+	 */
 	public Integer getDolor() {
 		return dolor;
 	}
 
+	/**
+	 * @param dolor
+	 *  resibe el valor para ser asignado al atributo dolos
+	 */
 	public void setDolor(Integer dolor) {
 		this.dolor = dolor;
 	}
