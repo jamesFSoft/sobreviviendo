d7b2257f97b000e9bc58c088111a49d6ae7d60a1
h3nryp3 <54733258+h3nryp3@users.noreply.github.com>
2021-06-14 22:14:05 -0500

    Comentarios para la clase Nocivo

--- src/application/clases/Nocivo.java
+++ src/application/clases/Nocivo.java
@@ -1,9 +1,17 @@
 package application.clases;
-
+/**
+    * Clase creada para el alimento Nocivo hereda de la Super class Alimento  
+*/
 public class Nocivo extends Alimento{
 	
+	/**
+	 * Atributos principales de la clase
+	 */
 	private Integer dolor;
 
+	/**
+        * Constructor de la clase, recibe parametros propios y heredados 
+    	*/
 	public Nocivo(String nombre, Integer puntaje, Integer dolor) {
 		super(nombre, puntaje);
 		this.puntaje = puntaje + dolor;
